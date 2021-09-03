bf3a6e3ae669929deccef828919dcc23147d5371
jcovington16 <covingtonjoshua7@gmail.com>
2021-09-02 21:06:07 -0600

    creating the workflow with the user interaction. will now work on the different actions that will take place if the user wants to play single player or muliplayer.

--- index.js
+++ index.js
@@ -9,37 +9,49 @@ function runGame() {
     newGame.startGame();
 
     console.log("What is your name?\n ")
-    let player_name = user_prompt();
+    let player_name = user_prompt() + '\n';
     let player1 = new human(player_name);
 
-    console.log("1: Single Player\n 2: Multiplayer\n 3: Exit")
+    console.log("1: Single Player\n2: Multiplayer\n3: Exit")
     let player_option = user_prompt();
-    let game_option = gameOption(player_option)
+    let game_option = gameOption(player_option, player1)
 
     return game_option
 }
 
-function gameOption(option) {
+function gameOption(option, player) {
     switch(option) {
         case '1':
+            singlePlayerGame(player);
+            break;
 
+        case '2':
+            multiPlayerGame(player);
+            break;
+
+        case '3':
+            console.log('Exiting...')
+            break;
     }
 }
 
-function singlePlayerGame(){
+function singlePlayerGame(player){
+    let computer = new ai('Computer');
+    
+    console.log("Chose your gesture:\n 1: Rock\n 2: Paper\n 3: Scissors\n 4: Lizard\n 5: Spock\n")
 
+    let user_gesture = user_prompt();
+    let user_results = player.getGesture(user_gesture)
+    
+    console.log(user_results);
 }
 
 function multiPlayerGame(){
-    
+
 }
 
-console.log("Chose your gesture:\n 1: Rock\n 2: Paper\n 3: Scissors\n 4: Lizard\n 5: Spock\n")
 
-let user_gesture = user_prompt();
-let user_results = player1.getGesture(user_gesture)
 
-console.log(user_results);
 
 runGame();
 
