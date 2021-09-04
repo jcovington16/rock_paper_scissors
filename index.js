1e33f30d449d96817209ae2edd39174d77e4eb12
jcovington16 <covingtonjoshua7@gmail.com>
2021-09-03 21:18:19 -0600

    Changed the workflow of the program. Instead of asking for the user's information in the beginning I decided that the user should chose which game mode they want to play and then add in their information. Will now start working out the logic on which gestures are better than others.

--- index.js
+++ index.js
@@ -9,26 +9,22 @@ function runGame() {
     let newGame = new game();
     newGame.startGame();
 
-    console.log("What is your name?\n ")
-    let player_name = user_prompt() + '\n';
-    let player1 = new human(player_name);
-
-    console.log("1: Single Player\n2: Multiplayer\n3: Exit")
+    console.log("1: Single Player\n2: Multiplayer\n3: Exit\n")
     let player_option = user_prompt();
-    let game_option = gameOption(player_option, player1)
+    let game_option = gameOption(player_option)
 
     return game_option
 }
 
 // allows the user to chose which game option they want
-function gameOption(option, player) {
+function gameOption(option) {
     switch(option) {
         case '1':
-            singlePlayerGame(player);
+            singlePlayerGame();
             break;
 
         case '2':
-            multiPlayerGame(player);
+            multiPlayerGame();
             break;
 
         case '3':
@@ -39,26 +35,47 @@ function gameOption(option, player) {
 
 // Single Player Mode
 function singlePlayerGame(player){
+
+    console.log("What is your name?\n")
+    let player_name = user_prompt() + '\n\n';
+    let player1 = new human(player_name);
+
     let computer = new ai('Computer');
     
     console.log("Chose your gesture:\n 1: Rock\n 2: Paper\n 3: Scissors\n 4: Lizard\n 5: Spock\n")
 
-    let user_gesture = user_prompt();
-    let user_results = player.getGesture(user_gesture)
+    let user_option = user_prompt();
+    let user_gesture = player1.getGesture(user_option)
 
     let computer_gesture = computer.getGesture();
+
+    //let resutls = checkGestures(user_gesture, computer_gesture)
     
-    console.log(user_results);
-    console.log(computer_gesture);
+    console.log(user_gesture);
+    console.log(computer_gesture)
 }
 
 // Multi Player Mode
 function multiPlayerGame(){
+    console.log("Multiplayer Mode Selected....\nPlayer 1 enter your name\n")
+
+    let player1_name = user_prompt();
+
+    console.log("Player 2 please enter your name...");
+
+    let player2_name = user_prompt();
+
+    let player1 = new human(player1_name);
+    let player2 = new human(player2_name);
+
+    console.log(player1);
+    console.log(player2);
+
 
 }
 
 // Checks the gesture and returns the results on who won.
-function checkGestures(gesture1, guesture2) {
+function checkGestures(gesture1, gesture2) {
 
 }
 
